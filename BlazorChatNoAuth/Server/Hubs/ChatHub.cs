f3679e5d72115a1962767696a4d512ee08b24a20
niklas-hjelm <niklas.hjelm@iths.se>
2023-01-24 15:55:32 +0100

    Add project files.

---
+++ BlazorChatNoAuth/Server/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChatNoAuth.Server.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string name, string message)
+    {
+        await Clients.All.SendAsync("SendMessage", name, message);
+    }
+}
